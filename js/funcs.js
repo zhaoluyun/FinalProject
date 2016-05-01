50256b4941c09914e1a55a04cae1ba2acc0dab98
zhaoluyun <zhaoluyun@ZhaoLuyundeMacBook.local>
2016-05-01 12:42:54 -0400

    edit

--- js/funcs.js
+++ js/funcs.js
@@ -7,21 +7,67 @@ function pointsWithin(rect) {
   var sw = rect[0];
   var ne = rect[2];
 
-  var sql = 'SELECT * FROM storefront_improvement WHERE the_geom @ ST_MakeEnvelope(' +
+  var sql = 'SELECT * FROM housing_final WHERE the_geom @ ST_MakeEnvelope(' +
     sw.lng + ','+ sw.lat + ',' + ne.lng + ',' + ne.lat + ', 4326)';
 
   $.ajax('https://zhaoluyun.cartodb.com/api/v2/sql/?q=' + sql).done(function(results) {
-    //console.log('pointsWithin:', results);
+    var value = _.map(results.rows,function(value){return value.price;});
+    var sum = _.reduce(value,function(memo,num){return memo+num;},0);
+    var average = sum / value.length;
+    console.log(average);
+    $('#average').empty();
+    $('#average').addClass("alert alert-success");
+    $('#average').append('The average price of the housing within this area is '+ parseInt(average) + ' Yuan/Square Meters');
     addRecords(results);
   });
 }
 
 
 
-/** Filter by numeric-input*/
-var sublayer2= [];
+function nClosest(point, n) {
+  var sql = 'SELECT * FROM housing_final ORDER BY the_geom <-> ST_SetSRID(ST_MakePoint(' + point[1] + ',' + point[0] + '),4326) ASC LIMIT ' + n;
+console.log(sql);
+  cartodb.createLayer(map, layerUrl3)
+    .addTo(map)
+    .on('done', function(layer) {
+      sublayer3 = layer.getSubLayer(0);
+      sublayer3.set({
+        sql: sql,
+        cartocss: "#layer { marker-fill: #5d4c52 }",
+      });
+    });
 
-$( "#go" ).click(function() {
+  $.ajax('https://zhaoluyun.cartodb.com/api/v2/sql/?q=' + sql).done(function(results) {
+    var value = _.map(results.rows,function(value){return value.price;});
+    var sum = _.reduce(value,function(memo,num){return memo+num;},0);
+    var average = sum / value.length;
+    console.log(average);
+    $('#average').empty();
+    $('#average').addClass("alert alert-success");
+    $('#average').append('The average price of the housing near this school is '+ average + ' Yuan/Square Meters');
+    addRecords(results);
+  });
+}
+
+function nClosest2(point, n) {
+  var sql = 'SELECT * FROM housing_final ORDER BY the_geom <-> ST_SetSRID(ST_MakePoint(' + point.lng + ',' + point.lat + '),4326) ASC LIMIT ' + n;
+  $.ajax('https://zhaoluyun.cartodb.com/api/v2/sql/?q=' + sql).done(function(results) {
+    var value = _.map(results.rows,function(value){return value.price;});
+    var sum = _.reduce(value,function(memo,num){return memo+num;},0);
+    var average = sum / value.length;
+    console.log(average);
+    $('#average').empty();
+    $('#average').addClass("alert alert-success");
+    $('#average').append('The average price of the housing near this point is '+ average + ' Yuan/Square Meters');
+    addRecords(results);
+  });
+}
+
+
+
+$( "#go1" ).click(function() {
+sublayer.hide();
+  if(sublayer2[0]!==1){sublayer2.hide();}
   var sql = 'SELECT * FROM housing_final WHERE (price >= ' + $('#numeric-input1').val() + ' AND price < ' + $('#numeric-input2').val() + ')';
   console.log(sql);
   cartodb.createLayer(map, layerUrl)
@@ -30,50 +76,122 @@ $( "#go" ).click(function() {
       sublayer2 = layer.getSubLayer(0);
       sublayer2.set({
         sql: sql,
-        cartocss: "#layer { marker-fill: red }",
+        cartocss: "#layer { marker-fill: #f4b657 }",
       });
 
       sublayer2.on('featureClick', function(e, latlng, pos, data) {});
     }).on('error', function(err) {
       // console.log(err):
     });
-
 });
 
-$( "#clear" ).click(function() {
-sublayer2.remove();
-$('#project-list').text('');
+$( "#clear1" ).click(function() {
+sublayer.show();
+    if(sublayer2[0]!==1){sublayer2.hide();}
 });
 
-var layer1 = [];
+
+$( "#go2" ).click(function() {
+  sublayer.hide();
+  if(sublayer4[0]!==1){sublayer4.hide();}
+  var sql = 'SELECT * FROM housing_final WHERE (distance_subwaystation >= ' + $('#numeric-input3').val()/100000 + ' AND distance_subwaystation < ' + $('#numeric-input4').val()/100000 + ')';
+  console.log(sql);
+  cartodb.createLayer(map, layerUrl)
+    .addTo(map)
+    .on('done', function(layer) {
+      sublayer4 = layer.getSubLayer(0);
+      sublayer4.set({
+        sql: sql,
+        cartocss: "#layer { marker-fill: yellow }",
+      });
+
+      sublayer4.on('featureClick', function(e, latlng, pos, data) {});
+    }).on('error', function(err) {
+      // console.log(err):
+    });
+});
+
+$( "#clear2" ).click(function() {
+  sublayer.show();
+    if(sublayer4[0]!==1){sublayer4.hide();}
+});
 
 
+$( "#go3" ).click(function() {
+  sublayer.hide();
+  if(sublayer5[0]!==1){sublayer5.hide();}
+  var sql = 'SELECT * FROM housing_final WHERE (distance_keyprimary >= ' + $('#numeric-input5').val()/100000 + ' AND distance_keyprimary < ' + $('#numeric-input6').val()/100000 + ')';
+  console.log(sql);
+  cartodb.createLayer(map, layerUrl)
+    .addTo(map)
+    .on('done', function(layer) {
+      sublayer5 = layer.getSubLayer(0);
+      sublayer5.set({
+        sql: sql,
+        cartocss: "#layer { marker-fill: #8dbeb1 }",
+      });
+
+      sublayer5.on('featureClick', function(e, latlng, pos, data) {});
+    }).on('error', function(err) {
+      // console.log(err):
+    });
+});
 
 
+$( "#clear3" ).click(function() {
+  sublayer.show();
+    if(sublayer5[0]!==1){sublayer5.hide();}
+});
 
-/**
- * function for adding one record
- *
- * The pattern of writing the function which solves for 1 case and then using that function
- *  in the definition of the function which solves for N cases is a common way to keep code
- *  readable, clean, and think-aboutable.
+$( "#go4" ).click(function() {
+  sublayer.hide();
+  if(sublayer6[0]!==1){sublayer6.hide();}
+  var sql = 'SELECT * FROM housing_final WHERE (distance_keymiddle >= ' + $('#numeric-input7').val()/100000 + ' AND distance_keymiddle < ' + $('#numeric-input8').val()/100000 + ')';
+  console.log(sql);
+  cartodb.createLayer(map, layerUrl)
+    .addTo(map)
+    .on('done', function(layer) {
+      sublayer6 = layer.getSubLayer(0);
+      sublayer6.set({
+        sql: sql,
+        cartocss: "#layer { marker-fill: #f1625d }",
+      });
+
+      sublayer6.on('featureClick', function(e, latlng, pos, data) {});
+    }).on('error', function(err) {
+      // console.log(err):
+    });
+});
+
+$( "#clear4" ).click(function() {
+  sublayer.show();
+    if(sublayer6[0]!==1){sublayer6.hide();}
+});
 
 function addOneRecord(rec) {
   var name = $('<p></p>')
-    .text('Name: ' + rec.business_name);
-
-  var corridor = $('<p></p>')
-    .text('Location: ' + rec.corridor);
-
-  var check_amount = $('<p></p>')
-    .text('Check Amount: ' + rec.check_amount);
-
-
+    .text('Name: ' + rec.name);
+  var area = $('<p></p>')
+      .text('Area: ' + rec.area +' Square Meters');
+  var totalprice = $('<p></p>')
+    .text('Total Price: ' + rec.totalprice +'0 Thouand Yuan');
+  var yearbuilt = $('<p></p>')
+    .text('Year of Construction: ' + rec.yearbuilt);
+  var subway = $('<p></p>')
+      .text('Distance to Subway Station: ' + parseInt(rec.distance_subwaystation*100000) +' Meters');
+  var primary = $('<p></p>')
+      .text('Distance to Top Primary School: ' + parseInt(rec.distance_keyprimary*100000) +' Meters');
+  var middle = $('<p></p>')
+      .text('Distance to Top Middle School: ' + parseInt(rec.distance_keymiddle*100000) +' Meters');
   var recordElement = $('<li></li>')
-    .addClass('list-group-item')
+    .addClass('list-group-item list-group-item-info')
     .append(name)
-    .append(corridor)
-    .append(check_amount);
+    .append(area)
+    .append(totalprice)
+    .append(yearbuilt)
+    .append(subway)
+    .append(primary)
+    .append(middle);
 
   $('#project-list').append(recordElement);
 }
@@ -82,4 +200,3 @@ function addRecords(cartodbResults) {
   $('#project-list').empty();
   _.each(cartodbResults.rows, addOneRecord);
 }
- */
--- js/setup.js
+++ js/setup.js
@@ -16,8 +16,15 @@ var Stamen_TonerLite = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/ton
 
 // The viz.json output by publishing on cartodb
 var layerUrl = 'https://zhaoluyun.cartodb.com/api/v2/viz/19eb8962-fff5-11e5-b571-0e787de82d45/viz.json';
-
+var layerUrl2 = 'https://zhaoluyun.cartodb.com/api/v2/viz/8e41b432-fff8-11e5-9754-0e787de82d45/viz.json';
+var layerUrl3 = 'https://zhaoluyun.cartodb.com/api/v2/viz/acfc0e5e-0edf-11e6-bd7e-0e787de82d45/viz.json';
+var click = 0;
 var sublayer = [];
+var sublayer2= [1];
+var sublayer3= [1];
+var sublayer4= [1];
+var sublayer5= [1];
+var sublayer6= [1];
 // Use of CartoDB.js
 cartodb.createLayer(map, layerUrl)
   .addTo(map)
@@ -31,64 +38,113 @@ cartodb.createLayer(map, layerUrl)
 
 
 
+var layer1 = [1];
   $( "#school" ).click(function() {
-
+    if(sublayer2[0]!==1){sublayer2.hide();}
+    if(sublayer4[0]!==1){sublayer4.hide();}
+    if(sublayer5[0]!==1){sublayer5.hide();}
+    if(sublayer6[0]!==1){sublayer6.hide();}
+    $('#title').text('Explore School District Housing');
+    $('#subtitle').text('Click school to see the houses surrounding it');
+    $('#mainpage').hide();
     sublayer.hide();
+    $('#project-list').text('');
+    $('#average').empty();
+    $('#average').removeClass("alert alert-success");
+    if (drawnLayerID !== 0) { map.removeLayer(map._layers[drawnLayerID]); drawnLayerID=0;}
+    if(layer1[0] !== 1 ){layer1.remove();}
     $('.cartodb-legend-stack').hide();
 
-    var layerUrl2 = 'https://zhaoluyun.cartodb.com/api/v2/viz/8e41b432-fff8-11e5-9754-0e787de82d45/viz.json';
+
     cartodb.createLayer(map, layerUrl2)
       .addTo(map)
       .on('done', function(layer) {
         layer.on('featureClick', function(e, latlng, pos, data) {
+          if(sublayer3[0]!==1){sublayer3.hide();}
+          nClosest(latlng, 20);
         });
-        layer1 = layer
+        layer1 = layer;
       }).on('error', function(err) {
       });
 
+      if(click!==0){map.removeControl(drawControl);click=0;}
   });
 
 
+  // Leaflet draw setup
+  var drawnItems = new L.FeatureGroup();
+  map.addLayer(drawnItems);
+
+  // Initialise the draw control and pass it the FeatureGroup of editable layers
+  var drawControl = new L.Control.Draw({
+    edit: {
+      featureGroup: drawnItems
+    },
+    draw: {
+      polyline: false,
+      polygon: false,
+      circle: false
+    },
+  });
+var drawnLayerID = 0;
 
 
   $( "#draw" ).click(function() {
-    sublayer.show();
-    layer1.remove();
+    click = click+1;
+    console.log(click);
+      if(sublayer3[0]!==1){sublayer3.hide();}
+      if(sublayer2[0]!==1){sublayer2.hide();}
+      if(sublayer4[0]!==1){sublayer4.hide();}
+      if(sublayer5[0]!==1){sublayer5.hide();}
+      if(sublayer6[0]!==1){sublayer6.hide();}
+      sublayer.show();
+      $('#project-list').text('');
+      $('#average').empty();
+      $('#average').removeClass("alert alert-success");
+      if(layer1[0] !== 1 ){layer1.remove();}
     $('.cartodb-legend-stack').show();
     $('.cartodb-legend.custom').hide();
+    $('#title').text('Explore by Personal Drawing');
+    $('#subtitle').text('Draw a point to see the housing that surrounding it. Draw a polygon to see the housing within it.');
+    $('#mainpage').hide();
 
-    // Leaflet draw setup
-    var drawnItems = new L.FeatureGroup();
-    map.addLayer(drawnItems);
-
-    // Initialise the draw control and pass it the FeatureGroup of editable layers
-    var drawControl = new L.Control.Draw({
-      edit: {
-        featureGroup: drawnItems
-      },
-      draw: {
-        polyline: false,
-        polygon: false,
-        circle: false,
-        marker: false
-      }
-    });
-
-
-    var drawnLayerID;
+    if(click!==1){map.removeControl(drawControl);click=click-1;}
     map.addControl(drawControl);
+
     map.on('draw:created', function (e) {
       var type = e.layerType;
       var layer = e.layer;
 
-
       if (type === 'rectangle') {
         pointsWithin(layer._latlngs);
-      }
-
-      if (drawnLayerID) { map.removeLayer(map._layers[drawnLayerID]); }
+      } else if (type === 'marker') {
+    nClosest2(layer._latlng, 20);
+  }
+      if (drawnLayerID!==0) { map.removeLayer(map._layers[drawnLayerID]); drawnLayerID=0;}
       map.addLayer(layer);
       drawnLayerID = layer._leaflet_id;
     });
+
+  });
+
+
+  $( "#main" ).click(function() {
+    if(sublayer3[0]!==1){sublayer3.hide();}
+    if(sublayer2[0]!==1){sublayer2.hide();}
+    if(sublayer4[0]!==1){sublayer4.hide();}
+    if(sublayer5[0]!==1){sublayer5.hide();}
+    if(sublayer6[0]!==1){sublayer6.hide();}
+    if(click!==0){map.removeControl(drawControl);click=0;}
+    $('#title').text('Second Hand Housing Sale Record in Beijing October 2014');
+    $('#subtitle').text('');
+    $('#project-list').text('');
+    $('#average').empty();
+    $('#average').removeClass("alert alert-success");
+    if (drawnLayerID!==0) { map.removeLayer(map._layers[drawnLayerID]); drawnLayerID=0;}
+      $('#mainpage').show();
+    sublayer.show();
+    if(layer1[0] !== 1 ){layer1.remove();}
+    $('.cartodb-legend-stack').show();
+    $('.cartodb-legend.custom').hide();
 
   });
